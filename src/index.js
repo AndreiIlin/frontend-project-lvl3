603c7d42ee1c6429a320dda5bb16974e84e761c0
AndreiIlin <andrei362065@gmail.com>
2022-04-15 03:08:06 +0300

    for checking #4

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import runApp from './init.js';
 
-runApp();
+document.addEventListener('DOMContentLoaded', () => {
+  runApp();
+})
