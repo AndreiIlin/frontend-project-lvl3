88aa28e2de8c8c4b088c9b92529ff866cf71aa55
AndreiIlin <andrei362065@gmail.com>
2022-04-16 14:20:22 +0300

    for checking #17

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default (link, i18nextInstance) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(`${link}`)}`)
+export default (link, i18nextInstance) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${link}`)
   .catch(() => {
     throw new Error(i18nextInstance.t('errors.networkError'));
   });
